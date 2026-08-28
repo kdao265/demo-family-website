5e764dec690a6fa68acd676f9ee4b589c128fb94
kdao265 <khanhdaoduy2k7@gmail.com>
2026-08-29 03:31:54 +0700

    Add Guestbook route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
@@ -10,19 +5,23 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Story from './pages/Story';
 import Gallery from './pages/Gallery';
+import Guestbook from './pages/Guestbook';
 
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
+
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/story" element={<Story />} />
             <Route path="/gallery" element={<Gallery />} />
+            <Route path="/guestbook" element={<Guestbook />} />
           </Routes>
         </main>
+
         <Footer />
       </div>
     </Router>
