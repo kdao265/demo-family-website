1156adea8c51ea69f70733116b01ad977d15f835
kdao265 <khanhdaoduy2k7@gmail.com>
2026-08-29 05:49:47 +0700

    Fix formatting issue in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export default function App() {
                   <Families />
                 </AdminGuard>
               }
-            
+            />
 
               <Route
                 path="/admin/members"
